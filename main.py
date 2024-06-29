4cb0b284c2c6ce41991bc9b406fae6fc7b61a7c2
PikaStars <pikachustarsza@gmail.com>
2024-06-28 23:18:18 -0700

    commit4

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ def main():
     Code Your Program here
     ########################################
     """
-    prev = int(input("Enter a number"))
+    prev = int(input("Enter a number:"))
     while True:
         num = int(input("Enter a number:"))
         if num > prev:
             break
         else:
+            prev = num
             continue
+            
     ########################################
     # Do not delete the return statement
     ########################################
